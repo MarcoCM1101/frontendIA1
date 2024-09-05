124c736226677e62d45447b909607c4d1b5f504f
Marco Caudillo <88803666+MarcoCM1101@users.noreply.github.com>
2024-09-04 21:28:24 -0600

    Cambios

--- FrontIA1/src/App.jsx
+++ FrontIA1/src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
   useEffect(() => {
+    console.log("Se montó el componente");
+
     const handleScroll = () => {
-      console.log("Scrolling detected!");
+      console.log("Se hizo scroll");
     };
 
     window.addEventListener("scroll", handleScroll);
 
-    // Limpiar el eventListener cuando el componente se desmonta
+    // Limpiar el listener cuando el componente se desmonta
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -16,8 +19,31 @@ function App() {
 
   return (
     <>
-      <div style={{ height: "200vh", padding: "20px" }}>
-        <h1>Scroll down to see the console log</h1>
+      <div className="container">
+        <div className="spline-section">
+          <div className={`title-overlay`}>
+            <h1>SPACE TITANIC</h1>
+          </div>
+
+          <spline-viewer url="https://prod.spline.design/Sf2RIM4QpX2PAk2w/scene.splinecode"></spline-viewer>
+        </div>
+        <div className="new-section">
+          <h1>Esta es una nueva sección</h1>
+          <p>Aquí puedes agregar más contenido.</p>
+          {/* Añadimos más contenido para forzar el scroll */}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
       </div>
     </>
   );
